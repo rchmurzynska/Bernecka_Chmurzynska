04262b637d6de5fa09a0e98b1f8d6ee7ea87b255
Reese Cloud <roksana.chmurzynska@gmail.com>
2025-07-01 23:05:33 +0200

    hero movement

---
+++ Player.cs
@@ -0,0 +1,11 @@
+class Player : Character
+{
+    {
+        {Console.A, "moveLeft"},
+        { Console.D, "moveRight"},
+        { Console.W, "moveUp"},
+        { Console.S, "moveDown"}
+    
+    };
+
+}
