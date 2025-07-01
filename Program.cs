d538af312abc256019175d4889cba8494f50bddb
Reese Cloud <roksana.chmurzynska@gmail.com>
2025-07-01 16:21:21 +0200

    moving character

---
+++ Program.cs
@@ -0,0 +1,7 @@
+Consol.CursorVisible = false;
+
+Dctionary<string, Point> directionsMap = new Dictionary<string, Point>();
+directionsMap.Add("moveLeft", new Point(-1, 0));
+directionsMap.Add("moveRight", new Point(1, 0));
+directionsMap.Add("moveUp", new Point(0, -1));
+directionsMap.Add("moveDown", new Point(0, 1));
