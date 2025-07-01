7865da6dfb4c56fc6588f58a711a591576d4e5f3
Reese Cloud <roksana.chmurzynska@gmail.com>
2025-07-01 22:55:05 +0200

    hero positioning

--- Program.cs
+++ Program.cs
@@ -5,3 +5,11 @@ directionsMap.Add("moveLeft", new Point(-1, 0));
 directionsMap.Add("moveRight", new Point(1, 0));
 directionsMap.Add("moveUp", new Point(0, -1));
 directionsMap.Add("moveDown", new Point(0, 1));
+
+Point startingPoint = new Point(18, 1);
+Player hero = new Player("", "");
+hero.speed = 1;
+hero.position = startingPoint;
+
+List<Character> characters = new List<Character>();
+characters.Add(hero);
